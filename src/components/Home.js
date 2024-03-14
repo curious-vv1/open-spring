30d0d1a5c5bcabab7e2eeba70fe6b431f3eed657
vivek <vivekrealc2@gmail.com>
2024-03-15 02:38:54 +0530

    contri

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Vivek2101240 from "./developers/Vivek2101240";
+import Kriti2101066 from "./developers/Kriti2101066";
+
+const Home = () => {
+  return (
+    <div>
+      <Vivek2101240 />
+      <Kriti2101066 />
+    </div>
+  );
+};
+
+export default Home;
